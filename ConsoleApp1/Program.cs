b52d177146162e9127a7e4b9e483c5cb11d11810
sumeetbajaj9 <sumeetbajaj9@gmail.com>
2022-03-13 23:44:22 +0530

    2nd commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Sumeet Here !");
+            Console.WriteLine("Sumeet Bajaj --- xTesting Teamcity build !!!");
         }
     }
 }
